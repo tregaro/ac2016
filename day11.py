d5273e9a0d0085047cf00a1a0f4925e58a2f8461
Pierre Tregaro <pierre@tregaro.se>
2016-12-12 17:42:05 +0000

    [!] day11 WIP

--- day11.py
+++ day11.py
@@ -1,6 +1,68 @@
+import heapq
+from copy import deepcopy
 from itertools import permutations
 
 from day11_input import puzzle_input
+
+
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+
+    def empty(self):
+        return len(self.elements) == 0
+
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
+
+class FloorState(object):
+    def __init__(self, state, current_floor):
+        self.state = state
+        assert len(state) == 4
+        self.current_floor = current_floor
+
+    def valid_moves(self):
+        current_state, current_floor = self.state, self.current_floor
+        possible_states = []
+
+        new_floors = []
+        # going up if we can
+        if current_floor < 3:
+            new_floors.append(current_floor + 1)
+
+        # going down if we can
+        if current_floor > 0:
+            new_floors.append(current_floor - 1)
+
+        for new_floor in new_floors:
+            # moving one thing
+            for thing in current_state[current_floor]:
+                temp_floor_state = current_state[new_floor] + [thing]
+                if floor_is_valid(temp_floor_state):
+                    new_floor_state = deepcopy(self.state)
+                    new_floor_state[new_floor] = temp_floor_state
+                    new_floor_state[current_floor].remove(thing)
+                    possible_states.append(FloorState(new_floor_state, new_floor))
+
+            # moving two things
+            things_permutations = list(permutations(current_state[current_floor], 2))
+            for i in things_permutations:
+                thing_permutations = list(i)
+                if floor_is_valid(thing_permutations):
+                    temp_floor_state = current_state[new_floor] + thing_permutations
+                    if floor_is_valid(temp_floor_state):
+                        new_floor_state = deepcopy(self.state)
+                        new_floor_state[new_floor] = temp_floor_state
+                        new_floor_state[current_floor].remove(thing_permutations[0])
+                        new_floor_state[current_floor].remove(thing_permutations[1])
+                        possible_states.append(FloorState(new_floor_state, new_floor))
+
+        return possible_states
+
 
 GENERATOR = 'G'
 MICROCHIP = 'M'
@@ -67,35 +129,33 @@ def floor_is_valid(floor):
     return True
 
 
-def valid_moves(state, current_floor):
-    possible_states = []
+def heuristic(a, b):
+    return len(a.state[3]) - len(b.state[3])
 
-    new_floors = []
-    # going up if we can
-    if current_floor < 3:
-        new_floors.append(current_floor + 1)
 
-    # going down if we can
-    if current_floor > 0:
-        new_floors.append(current_floor - 1)
+def a_star_search(start, goal):
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
 
-    for new_floor in new_floors:
-        # moving one thing
-        for thing in state[current_floor]:
-            temp_floor_state = state[new_floor] + [thing]
-            if floor_is_valid(temp_floor_state):
-                possible_states.append(temp_floor_state)
+    while not frontier.empty():
+        current = frontier.get()
 
-        # moving two things
-        things_permutations = list(permutations(state[current_floor], 2))
-        for i in things_permutations:
-            thing_permutations = list(i)
-            if floor_is_valid(thing_permutations):
-                temp_floor_state = state[new_floor] + thing_permutations
-                if floor_is_valid(temp_floor_state):
-                    possible_states.append(temp_floor_state)
+        if current == goal:
+            break
 
-    return possible_states
+        for next in current.valid_moves():  # graph.neighbors(current):
+            new_cost = cost_so_far[current] + 1
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + heuristic(goal, next)
+                frontier.put(next, priority)
+                came_from[next] = current
+
+    return came_from, cost_so_far
 
 
 def normal():
@@ -114,7 +174,11 @@ def normal():
 
     # You start on the first floor
     current_floor = 0
-    valid_moves(floors, current_floor)
+    goal = [[], [], [], []]
+    for floor in floors:
+        goal[3].extend(floor)
+
+    print a_star_search(FloorState(floors, 0), FloorState(goal, 3))
 
     # Rule 1: Have to have at least one chip or generator to go in lift.
     # Rule 2: Chips will be fried if on the same floor as a generator unless it's their generator
